22d0952828ace24acef5a865113d29f593ef5415
priya <priyadaws86@gmail.com>
2026-05-04 15:32:09 +0530

    First commit in roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -25,18 +25,18 @@ aws route53 change-resource-record-sets \
   --hosted-zone-id $ZONE_ID \
   --change-batch '
   {
-    "Comment": "Testing creating a record set",
-    "Changes": [{
-      "Action": "UPSERT",
-      "ResourceRecordSet": {
-        "Name": "$RECORD_NAME",
-        "Type": "A",
-        "TTL": 1,
-        "ResourceRecords": [{
-            "Value": "${IP}"
+    "Comment": "Updating record set"
+    ,"Changes": [{
+      "Action"              : "UPSERT"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'RECORD_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$IP'"
         }]
       }
     }]
   }
-    '
+  '
 done
