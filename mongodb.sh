5bbb91514e773a7a3e28bc951512a7b293ffd1e6
priya <priyadaws86@gmail.com>
2026-05-04 17:29:37 +0530

    First commit in roboshop

--- mongodb.sh
+++ mongodb.sh
@@ -7,7 +7,7 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER="/var/log/shell-script"
+LOGS_FOLDER="/var/log/shell-roboshop"
 SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
@@ -41,4 +41,4 @@ VALIDATE(){
  systemctl start mongod &>>$LOG_FILE
  VALIDATE $? "Start MongoDB"
 
- 
+
