5c4a4c99841e889c69cfa6dc80be74ce047990bc
priya <priyadaws86@gmail.com>
2026-05-04 14:59:28 +0530

    First commit in roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -2,6 +2,8 @@
 
 AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-0b7b140563e1a1c2a"
+ZONE_ID="Z0472149H3YR900V88FM"
+DOMAIN_NAME="DAWS86.CLOUD
 
 for instance in $@
 do
@@ -11,12 +13,30 @@ INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro 
 
 if [ $instance != "frontend" ]; then
  IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
-
+RECORD_NAME="$instance.$DOMAIN_NAME"
 else
 IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
-
+RECORD_NAME="$DOMAIN_NAME"
 fi
 
 echo "$instance: $IP"
 
+aws route53 change-resource-record-sets \
+  --hosted-zone-id $ZONE_ID \
+  --change-batch '
+  {
+    "Comment": "Updating record set"
+    ,"Changes": [{
+      "Action"              : "upsert"
+      ,"ResourceRecordSet"  : {
+        "Name"              : "'$RECORD_NAME'"
+        ,"Type"             : "A"
+        ,"TTL"              : 1
+        ,"ResourceRecords"  : [{
+            "Value"         : "'$IP'"
+        }]
+      }
+    }]
+  }
+  '
 done
