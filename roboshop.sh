47a633d163e574fe6ee5bf55e42b51343edde4a5
priya <priyadaws86@gmail.com>
2026-05-04 14:18:03 +0530

    First commit in roboshop

--- roboshop.sh
+++ roboshop.sh
@@ -3,14 +3,13 @@
 AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-0b7b140563e1a1c2a"
 
-for instance in "$@"
+for instance in $@
 do
-INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value='"$instance"'}]' 
---query 'Instances[0].InstanceId' --output text)
+INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value='"$instance"'}]' --query 'Instances[0].InstanceId' --output text)
 
 # To get Private IP
 
-if [ "$instance" != "frontend" ]; then
+if [ $instance != "frontend" ]; then
  IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
 
 else
