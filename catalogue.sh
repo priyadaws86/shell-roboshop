06099708dddc913815d5c031b6c1d9fa327fe727
priya <priyadaws86@gmail.com>
2026-05-05 18:32:56 +0530

    Roboshop project

--- catalogue.sh
+++ catalogue.sh
@@ -44,8 +44,8 @@ dnf install nodejs -y &>>$LOG_FILE
 VALIDATE $? "Installing NodeJS" 
 
 id roboshop &>>$LOG_FILE
-if [ $? -ne 0]; then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE   
+if [ $? -ne 0 ]; then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
     VALIDATE $? "Creating a system user for roboshop"
  else
     echo -e "User already exists ... $Y SKIPPIN $N" | tee -a $LOG_FILE
