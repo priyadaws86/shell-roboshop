52a1f86fadddff9bf98320f7eef1306b3487544e
priya <priyadaws86@gmail.com>
2026-05-07 12:48:57 +0530

    Roboshop Project

--- user.sh
+++ user.sh
@@ -80,3 +80,9 @@ VALIDATE $? "Enabling user service"
 
 systemctl start user &>>$LOG_FILE
 VALIDATE $? "Starting User service"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+echo -e "Script executed in: $Y $TOTAL_TIME seconds $N"
+
+
