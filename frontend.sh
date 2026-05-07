458caa4f79f30ff6e5b1f9c2c73d0daba884fc1e
priya <priyadaws86@gmail.com>
2026-05-07 13:48:00 +0530

    Roboshop Project

---
+++ frontend.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
+START_TIME=$(date +%s)
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at : $(date)" | tee -a $LOG_FILE
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R Error:: Please run this script with root privilages $N"
+    exit 1
+fi  
+
+VALIDATE(){
+
+    if [ $1 -ne 0 ]; then
+        echo -e "$2 ... $R Failed $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$2 ... $G Successful $N" | tee -a $LOG_FILE
+    fi
+}
+
+dnf module disable nginx -y  &>>$LOG_FILE
+VALIDATE $? "Disabling existing Nginx module"
+
+dnf module enable nginx:1.24 -y &>>$LOG_FILE
+VALIDATE $? "Enabling Nginx module"
+
+dnf install nginx -y  &>>$LOG_FILE
+VALIDATE $? "Installing Nginx"
+
+systemctl enable nginx  &>>$LOG_FILE
+VALIDATE $? "Enabling Nginx service"
+
+systemctl start nginx  &>>$LOG_FILE
+VALIDATE $? "Starting Nginx service"
+
+rm -rf /usr/share/nginx/html/* 
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip 
+VALIDATE $? "Downloading and extracting Frontend code"
+
+rm -rf vim /etc/nginx/nginx.conf   
+VALIDATE $? "Removing existing Nginx configuration file"
+
+cp  $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf &>>$LOG_FILE
+VALIDATE $? "Copying Nginx configuration file"
+
+
+systemctl restart nginx  &>>$LOG_FILE
+VALIDATE $? "Restarting Nginx service"
+
+
+
+
