566c6bf523e1cef9f7d53e228dfdae857a42fb42
priya <priyadaws86@gmail.com>
2026-05-07 12:28:14 +0530

    Roboshop Project

--- redis.sh
+++ redis.sh
@@ -36,12 +36,12 @@ VALIDATE $? "Enabling Redis module"
 dnf install redis -y  &>>$LOG_FILE
 VALIDATE $? "Installing Redis"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/redis &>>$LOG_FILE
- VALIDATE $? "Allowing remote connections to Redis
+sed -i 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>>$LOG_FILE
+ VALIDATE $? "Allowing remote connections to Redis"
 
- systemctl enable redis
- VALIDATE $? "Enable Redis"
+systemctl enable redis &>>$LOG_FILE
+VALIDATE $? "Enabling Redis service"
 
- systemctl start redis
- VALIDATE $? "Start Redis"
+systemctl start redis &>>$LOG_FILE
+VALIDATE $? "Starting Redis service"
 
