a3cdad1442c06159ee824aae0d5a5a0b5eff064a
priya <priyadaws86@gmail.com>
2026-05-07 12:38:05 +0530

    Roboshop Project

--- redis.sh
+++ redis.sh
@@ -47,7 +47,7 @@ systemctl start redis &>>$LOG_FILE
 VALIDATE $? "Starting Redis service"
 
 END_TIME=$(date +%s)
-TOTAL_TIME=$((END_TIME - START_TIME))
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
 echo -e "Script executed in: $Y $TOTAL_TIME seconds $N"
 
